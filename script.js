79bd6eb514c7efe2cabc637fc250bb105f005afc
Jonatas Ferreira <jonatasdevcode@users.noreply.github.com>
2023-06-11 11:25:03 -0300

    Themed photo modification

--- script.js
+++ script.js
@@ -8,4 +8,15 @@ function toggleMode() {
 
   // simplifying the code
   html.classList.toggle("light")
+
+  // Pegar a tag img
+  const img = document.querySelector("#profile img")
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // Se tiver light mode, adicionat a imagem ligh;
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    // Se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "assets/Avatar.png")
+  }
 }
