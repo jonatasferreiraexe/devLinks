2706b5c54173f17911dfb7854d02e40583a3f86d
Jonatas Ferreira <jonatasdevcode@users.noreply.github.com>
2023-06-11 10:40:20 -0300

    simplifying the code

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function toggleMode() {
   const html = document.documentElement
-  if (html.classList.contains("light")) {
-    html.classList.remove("light")
-  } else {
-    html.classList.add("light")
-  }
+  // if (html.classList.contains("light")) {
+  //   html.classList.remove("light")
+  // } else {
+  //   html.classList.add("light")
+  // }
+
+  // simplifying the code
+  html.classList.toogle("light")
 }
